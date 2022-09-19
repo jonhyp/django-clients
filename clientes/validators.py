e15cccc375e8f841b4aa2e86202820ac7cb88c2a
jonhyp <jpbdvmc2@gmail.com>
2022-09-19 20:51:19 -0300

    Validacao de celular com expressoes regulares

--- clientes/validators.py
+++ clientes/validators.py
@@ -1,3 +1,5 @@
+import re
+
 def cpf_valido(cpf):
     return len(cpf) == 11
    
@@ -8,4 +10,7 @@ def rg_valido(rg):
     return len(rg) == 9
 
 def celular_valido(celular):
-    return len(celular) > 10
+    """""Verifica se o celular é valido (11 91234-1234)"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, celular)
+    return resposta
